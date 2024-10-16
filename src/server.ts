b7c2bf3321cd3acf29e0a86658034c45f0de82b7
Yash Mathur <yashmat05@gmail.com>
2024-10-16 12:42:01 -0700

    integrate with vercel

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,6 @@ import path from 'path';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
@@ -20,7 +19,7 @@ connectDB();
 // Middleware
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Use environment variable for frontend URL
   credentials: true
 }));
 app.use(express.json());
@@ -32,6 +31,4 @@ app.use('/api/recipients', recipientRoutes);
 app.use('/api/airtable', airtableRoutes);
 app.use('/api/emails', emailRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+export default app;
